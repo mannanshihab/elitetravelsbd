63a30c9223bf7f0c9470d14a71f1b5f0fa6002c3
mannanshihab <amannanshihab@gmail.com>
2024-07-14 19:18:38 +0600

    user-crud-modal

--- app/Livewire/UserList.php
+++ app/Livewire/UserList.php
@@ -2,13 +2,89 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Session;
 use Livewire\Attributes\Title;
 use Livewire\Component;
+use Livewire\WithPagination;
+
 #[Title('Users')]
 class UserList extends Component
 {
+    use WithPagination; 
+    public $user_id;
+    public $name;
+    public $email;
+    public $isOpen = 0;
+ 
+    public function create()
+    {
+        $this->openModal();
+    }
+    public function openModal()
+    {
+        $this->isOpen = true;
+    }
+    public function closeModal()
+    {
+        $this->isOpen = false;
+    }
+
     public function render()
     {
-        return view('livewire.user-list');
+        $users = User::all();
+        return view('livewire.user-list',[
+           'users' =>$users
+        ]);
     }
+
+    public function store()
+    {
+        $this->validate([
+            'name' => 'required',
+            'email' => 'required',
+        ]);
+
+        User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => bcrypt('password'),	
+        ]);
+        
+        Session::flash('success', 'User created successfully.');
+       
+        $this->reset('name','email');
+        $this->closeModal();
+    }
+
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+        $this->user_id = $id;
+        $this->name = $user->name;
+        $this->email = $user->email;
+
+        $this->openModal();
+    }
+    public function update()
+    {
+        if ($this->user_id) {
+            $user = User::findOrFail($this->user_id);
+            $user->update([
+                'name' => $this->name,
+                'email' => $this->email,
+                'password' => bcrypt('password'),
+            ]);
+            Session::flash('success', 'User updated successfully.');
+            $this->closeModal();
+            $this->reset('name', 'email', 'user_id');
+        }
+    }
+    public function delete($id)
+    {
+        User::find($id)->delete();
+        Session::flash('success', 'User deleted successfully.');
+        $this->reset('name','email');
+    }
+
 }
--- resources/views/livewire/user-list.blade.php
+++ resources/views/livewire/user-list.blade.php
@@ -1,118 +1,104 @@
 <div>
 
+  <h4 class="py-3 mb-4"><span class="text-muted fw-light">Users /</span> List</h4>
 
-<h4 class="py-3 mb-4"><span class="text-muted fw-light">DataTables /</span> Basic</h4>
+  <!-- DataTable with Buttons -->
+  <div class="card">
+    <div class="d-flex justify-content-between mt-5 m-3">
+      <button wire:click="create" class="btn btn-primary" data-bs-toggle="modal"> Add User </button>
+    </div>
+    <div class="px-5">
+      @include('livewire.partials.flash-session')
+    </div>
+    <div class="card-datatable table-responsive pt-0">
+      <table class="datatables-basic table">
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Status</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($users as $user)
+          <tr>
+            <td>{{ $user->id }}</td>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->email }}</td>
+            <td></td>
+            <td>
+              <div class="dropdown">
+                <button type="button" class="btn btn-primary text-center dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="ti ti-dots-vertical"></i>Action</button>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" wire:click="edit({{ $user->id }})" href="javascript:void(0);"><i class="ti ti-pencil me-1"></i> Edit</a>
+                  <a class="dropdown-item" onclick="return confirm('Are you sure you want to delete this item?') || event.stopImmediatePropagation()" wire:click="delete({{ $user->id }})" href="javascript:void(0);"><i class="ti ti-trash me-1"></i> Delete</a>
+                </div>
+              </div>
+            </td>
+          </tr>
+          @endforeach
+        </tbody>
+      </table>
+      {{-- <div>
+        {{ $users->links() }}
+      </div> --}}
+    </div>
+  </div>
 
-<!-- DataTable with Buttons -->
-<div class="card">
-  <div class="card-datatable table-responsive pt-0">
-    <table class="datatables-basic table">
-      <thead>
-        <tr>
-          <th></th>
-          <th></th>
-          <th>id</th>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Date</th>
-          <th>Salary</th>
-          <th>Status</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-    </table>
-  </div>
-</div>
-<!-- Modal to add new record -->
-<div class="offcanvas offcanvas-end" id="add-new-record">
-  <div class="offcanvas-header border-bottom">
-    <h5 class="offcanvas-title" id="exampleModalLabel">New Record</h5>
-    <button
-      type="button"
-      class="btn-close text-reset"
-      data-bs-dismiss="offcanvas"
-      aria-label="Close"></button>
-  </div>
-  <div class="offcanvas-body flex-grow-1">
-    <form class="add-new-record pt-0 row g-2" id="form-add-new-record" onsubmit="return false">
-      <div class="col-sm-12">
-        <label class="form-label" for="basicFullname">Full Name</label>
-        <div class="input-group input-group-merge">
-          <span id="basicFullname2" class="input-group-text"><i class="ti ti-user"></i></span>
-          <input
-            type="text"
-            id="basicFullname"
-            class="form-control dt-full-name"
-            name="basicFullname"
-            placeholder="John Doe"
-            aria-label="John Doe"
-            aria-describedby="basicFullname2" />
-        </div>
-      </div>
-      <div class="col-sm-12">
-        <label class="form-label" for="basicPost">Post</label>
-        <div class="input-group input-group-merge">
-          <span id="basicPost2" class="input-group-text"><i class="ti ti-briefcase"></i></span>
-          <input
-            type="text"
-            id="basicPost"
-            name="basicPost"
-            class="form-control dt-post"
-            placeholder="Web Developer"
-            aria-label="Web Developer"
-            aria-describedby="basicPost2" />
-        </div>
-      </div>
-      <div class="col-sm-12">
-        <label class="form-label" for="basicEmail">Email</label>
-        <div class="input-group input-group-merge">
-          <span class="input-group-text"><i class="ti ti-mail"></i></span>
-          <input
-            type="text"
-            id="basicEmail"
-            name="basicEmail"
-            class="form-control dt-email"
-            placeholder="john.doe@example.com"
-            aria-label="john.doe@example.com" />
-        </div>
-        <div class="form-text">You can use letters, numbers & periods</div>
-      </div>
-      <div class="col-sm-12">
-        <label class="form-label" for="basicDate">Joining Date</label>
-        <div class="input-group input-group-merge">
-          <span id="basicDate2" class="input-group-text"><i class="ti ti-calendar"></i></span>
-          <input
-            type="text"
-            class="form-control dt-date"
-            id="basicDate"
-            name="basicDate"
-            aria-describedby="basicDate2"
-            placeholder="MM/DD/YYYY"
-            aria-label="MM/DD/YYYY" />
-        </div>
-      </div>
-      <div class="col-sm-12">
-        <label class="form-label" for="basicSalary">Salary</label>
-        <div class="input-group input-group-merge">
-          <span id="basicSalary2" class="input-group-text"><i class="ti ti-currency-dollar"></i></span>
-          <input
-            type="number"
-            id="basicSalary"
-            name="basicSalary"
-            class="form-control dt-salary"
-            placeholder="12000"
-            aria-label="12000"
-            aria-describedby="basicSalary2" />
-        </div>
-      </div>
-      <div class="col-sm-12">
-        <button type="submit" class="btn btn-primary data-submit me-sm-3 me-1">Submit</button>
-        <button type="reset" class="btn btn-outline-secondary" data-bs-dismiss="offcanvas">Cancel</button>
-      </div>
-    </form>
-  </div>
-</div>
-<!--/ DataTable with Buttons -->
+  @if ($isOpen)
+  <!-- User Modal -->
+  <div class="modal show" tabindex="-1" aria-hidden="true" style="display: block;" >
+    <div class="modal-dialog modal-lg modal-simple modal-edit-user">
+      <div class="modal-content p-3 p-md-5">
+        <div class="modal-body">
+          <button wire:click="closeModal" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <div class="text-center mb-4">
+            <h3 class="mb-2"> {{$user_id ? 'Edit User' : 'Create User' }}</h3>
+            <p class="text-muted">Details will receive a privacy audit.</p>
+          </div>
+          <form wire:submit.prevent="{{ $user_id ? 'update' : 'store' }}" class="row g-3">
+            <div class="col-12">
+              <label class="form-label" for="Name">Name</label>
+              <input type="text" wire:model="name" id="name" name="Name" class="form-control" placeholder="user name" />
+            </div>
+            <div class="col-12 col-md-6">
+              <label class="form-label" for="Email">Email</label>
+              <input type="text" wire:model="email" id="email" name="email" class="form-control" placeholder="example@domain.com" />
+            </div>
+            <div class="col-12 col-md-6">
+              <label class="form-label" for="Password">Password</label>
+              <input type="password" wire:model="password" id="password" name="email" class="form-control" />
+            </div>
+            <div class="col-12 col-md-6">
+              <label class="form-label" for="Status">Status</label>
+              <select id="Status" name="Status" class="select2 form-select" aria-label="Default select example">
+                <option selected>Status</option>
+                <option value="1">Active</option>
+                <option value="2">Inactive</option>
+                <option value="3">Suspended</option>
+              </select>
+            </div>
+            
+            <div class="col-12 col-md-6">
+              <label class="form-label" for="modalEditUserPhone">Phone Number</label>
+              <div class="input-group">
+                <span class="input-group-text">US (+1)</span>
+                <input type="text" id="modalEditUserPhone" name="modalEditUserPhone" class="form-control phone-number-mask" placeholder="202 555 0111" />
+              </div>
+            </div>
 
+            <div class="col-12 text-center">
+              <button type="submit" class="btn btn-primary me-sm-3 me-1">{{ $user_id ? 'Update' : 'Create' }}</button>
+              <button type="reset" wire:click="closeModal" class="btn btn-label-secondary" data-bs-dismiss="modal" aria-label="Close">Cancel</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--/User Modal -->
+  @endif
 </div>
 
