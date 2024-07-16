d43cfd0527f27aaab5d247ef4cc9012e85683e2d
mannanshihab <amannanshihab@gmail.com>
2024-07-16 10:47:50 +0600

    update

--- app/Livewire/UserList.php
+++ app/Livewire/UserList.php
@@ -15,20 +15,6 @@ class UserList extends Component
     public $user_id;
     public $name;
     public $email;
-    public $isOpen = 0;
- 
-    public function create()
-    {
-        $this->openModal();
-    }
-    public function openModal()
-    {
-        $this->isOpen = true;
-    }
-    public function closeModal()
-    {
-        $this->isOpen = false;
-    }
 
     public function render()
     {
@@ -38,48 +24,6 @@ class UserList extends Component
         ]);
     }
 
-    public function store()
-    {
-        $this->validate([
-            'name' => 'required',
-            'email'    => 'required|email|unique:users|max:255',
-        ]);
-
-        User::create([
-            'name' => $this->name,
-            'email' => $this->email,
-            'password' => bcrypt('password'),	
-        ]);
-        
-        Session::flash('success', 'User created successfully.');
-       
-        $this->reset('name','email');
-        $this->closeModal();
-    }
-
-    public function edit($id)
-    {
-        $user = User::findOrFail($id);
-        $this->user_id = $id;
-        $this->name = $user->name;
-        $this->email = $user->email;
-
-        $this->openModal();
-    }
-    public function update()
-    {
-        if ($this->user_id) {
-            $user = User::findOrFail($this->user_id);
-            $user->update([
-                'name' => $this->name,
-                'email' => $this->email,
-                'password' => bcrypt('password'),
-            ]);
-            Session::flash('success', 'User updated successfully.');
-            $this->closeModal();
-            $this->reset('name', 'email', 'user_id');
-        }
-    }
     public function delete($id)
     {
         User::find($id)->delete();
